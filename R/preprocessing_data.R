04555ba9b09ba4150001d21d54fe45266e0709fa
Sebastian Palomino <sebastian.palomino@sei.org>
2025-07-04 16:30:26 -0500

    Insert new parameter in preprocessing function

--- R/preprocessing_data.R
+++ R/preprocessing_data.R
@@ -4,11 +4,12 @@
 #' @param aligned 
 #' @param index 
 #' @param stations 
+#' @param timestep 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-preprocessing_data <- function(stack.radar, aligned, index, stations){
+preprocessing_data <- function(stack.radar, aligned, index, stations, timestep){
   
 }
