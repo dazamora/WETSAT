c6c4cc467ed8324736479947f92040f6606a56c3
David Zamora <dazamoraa@unal.edu.co>
2025-07-22 10:55:18 -0500

    Major changes in radar_index_stack function.

--- R/radar_index_stack.R
+++ R/radar_index_stack.R
@@ -1,16 +1,106 @@
-#' Title
+#' @name 
+#' radar_index_stack
+#' 
+#' @title 
+#' Indexes stack of backscatter and data frame with indexes values by hydrology station
 #'
-#' @param VV Is the backscatter of the signal transmited and recived vertical
+#' @param VV is the backscatter of the signal transmitted and recived vertical
 #' @param VH 
-#' @param index 
+#' @param index  \cite{(Mayer et al., 1974)}
 #' @param nameID 
 #'
 #' @return
 #' @export
+#' 
+#' @author David Zamora <david.zamora@sei.org> \cr
+#' Sebastian Palomino <sebastian.palomino@sei.org> \cr
 #'
+#' @references reference
+#' \itemize{
+#'  \item Mayer, T., Poortinga, A., Bhandari, B., Nicolau, A. P., Markert, K., Thwal, N. S., ... & Saah, D. (2021). Deep learning approach for Sentinel-1 surface water mapping leveraging Google Earth Engine. \emph{ISPRS Open Journal of Photogrammetry and Remote Sensing}, 2, 100005. \doi{10.1016/j.ophoto.2021.100005}  
+#'  \item Huang, W., DeVries, B., Huang, C., Lang, M.W., Jones, J.W., Creed, I.F., Carroll, M.L. (2018). Automated extraction of surface water extent from sentinel-1 data. \emph{Rem. Sens.} 10, 797. \emph{10.3390/rs10050797}
+#' }
+#' 
 #' @examples
 #' 
-radar_index_stack <- function(VV, VH, index, nameID){
-  salida <- VV+VH
-  return(salida)
+radar_index_stack <- function(VV, VH, index, nameID, station, output, buff){
+  
+  if(!(raster::nlayers(VV) == raster::nlayers(VH))){
+    cat("Backscatter variables have same number of layers")
+  } else {
+    stop("Backscatter variables have different number of layers or VV-VH are not raster format")  
+  }
+  
+  if(exists("index")){
+    if(is.character(index)){
+        cat(paste("The following indices will be calculated: ", index, sep = ""))
+    } else {
+      stop("The index must be a vector of characters with at least a the index name")
+    }
+  } else {
+    index <- c("PR", "NDPI", "NVHI", "NVVI", "RVI")
+  }
+  
+  if(raster::nlayers(VV) > 1){
+    if(!class(VV)[1] == "RasterStack"){
+      
+    }
+      
+      
+  } else {
+    if(length(index) == 5){
+      
+      PR <- VH/VV
+      NDPI <- (VV - VH)/(VV + VH)
+      NVHI <- VH/(VV + VH)
+      NVVI <- VV/(VV + VH)
+      RVI <- 4*VH/(VV + VH)
+      out.indexes <- list(PR = PR, NDPI = NDPI, NVHI = NVHI, NVVI = NVVI, RVI = RVI)
+    } else {
+      
+      PR <- VH/VV
+      NDPI <- (VV - VH)/(VV + VH)
+      NVHI <- VH/(VV + VH)
+      NVVI <- VV/(VV + VH)
+      RVI <- 4*VH/(VV + VH)
+      
+      nam.ind <- c("PR", "NDPI", "NVHI", "NVVI", "RVI")
+      select.ind <- is.element(nam.ind, index)
+      
+      out.indexes <- list(PR = PR, NDPI = NDPI, NVHI = NVHI, NVVI = NVVI, RVI = RVI)[select.ind]
+    }
+  }
+  
+  points_vect.rp <- st_transform(points_vect, crs = "+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs")
+  
+  
+  if(exists("station")){
+    
+    if(inherits(station, "sf") & unique(sf::st_is(station, "POINT"))){
+      
+      if(sf::st_crs(VV) == sf::st_crs(station)){
+        print("salida")
+      } else {
+        station <- sf::st_transform(station, crs = sf::st_crs(VV))
+      }
+        
+    } else {
+      stop("The variable station does not have the structure of the sf package or is not a station point or point set.")
+    }
+    
+    extract.index.buff <- lapply(out.indexes, function(x, y, w) raster::extract(x, y, buffer = w), y = station, w = buff)
+    median.extract <- lapply(extract.index.buff, function(x) lapply(x, function(z) apply(z, 2, median, na.rm = TRUE)))
+    convert.data.frame <- lapply(median.extract, function(x) do.call(rbind, x))
+    
+    coord.station <- sf::st_coordinates(station)
+    result.station.indexes <- lapply(convert.data.frame, function(x, y) cbind(y, x), y = coord.station)
+    
+  } else {
+    cat("There are not a station variable to extract data from stack indexes")
+  }
+  
+  output <- list(stack.index = out.indexes, matrix.index.station = result.station.indexes)
+  return(output)
 }
+
+
